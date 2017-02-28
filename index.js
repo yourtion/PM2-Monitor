4b4c909f14d0c315c85bf4080fecababa6c75546
YourtionGuo <yourtion@gmail.com>
2017-02-28 13:59:58 +0800

    eslint

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-var pmx = require('pmx');
-var rpc = require('pm2-axon-rpc');
-var axon = require('pm2-axon');
-var net = require('net');
-var utils = require('./utils');
-var debug = utils.debug('model');
+const pmx = require('pmx');
+const rpc = require('pm2-axon-rpc');
+const axon = require('pm2-axon');
+const net = require('net');
+const utils = require('./lib/utils');
+const debug = utils.debug('model');
 
-var PROCESS_NAME = 'pm2-monit-daemon';
-var PM2_ROOT_PATH = utils.getPM2home();
-var RPC_PATH = 'unix://' + PM2_ROOT_PATH + '/rpc.sock';
-var PUB_PATH = 'unix://'+ PM2_ROOT_PATH +'/pub.sock'
-var Probe = pmx.probe();
-var req = axon.socket('req');
-var rpcClient = new rpc.Client(req);
-var SOCKET = new net.Socket();
+const PROCESS_NAME = 'pm2-monit-daemon';
+const PM2_ROOT_PATH = utils.getPM2home();
+const RPC_PATH = 'unix://' + PM2_ROOT_PATH + '/rpc.sock';
+const PUB_PATH = 'unix://' + PM2_ROOT_PATH + '/pub.sock';
+const Probe = pmx.probe();
+const req = axon.socket('req');
+const rpcClient = new rpc.Client(req);
+const SOCKET = new net.Socket();
 
-var conf = pmx.initModule({
+const conf = pmx.initModule({
   widget: {
     logo: 'https://app.keymetrics.io/img/logo/keymetrics-300.png',
-    theme: ['#141A1F', '#222222', '#3ff', '#3ff'],
-    el : { probes: true, actions: true },
-    block : { actions: false, issues: true, meta : true, main_probes: ['test-probe']},
-  }
+    theme: [ '#141A1F', '#222222', '#3ff', '#3ff' ],
+    el: { probes: true, actions: true },
+    block: { actions: false, issues: true, meta: true, main_probes: [ 'test-probe' ]},
+  },
 });
 
 // default: 30 secs
-var WORKER_INTERVAL = isNaN(parseInt(conf.workerInterval)) ? 30 * 1000 : parseInt(conf.workerInterval) * 1000;
+const WORKER_INTERVAL = isNaN(parseInt(conf.workerInterval, 10)) ? 30 * 1000 : parseInt(conf.workerInterval, 10) * 1000;
 
-SOCKET.on('connect' ,function() {
+SOCKET.on('connect', function () {
   debug('CONNECTED TO:' + SOCKET.remoteAddress + ':' + SOCKET.remotePort);
   req.connect(RPC_PATH);
   sendSystemData();
 });
 
-SOCKET.on('data', function(data) {
+SOCKET.on('data', function (data) {
   debug('DATA:' + data);
 });
 
-SOCKET.on('close', function() {
+SOCKET.on('close', function () {
   debug('Connection closed');
 });
 
-SOCKET.on('error', function(error) {
+SOCKET.on('error', function (error) {
   debug('ERROR:' + error);
 });
 
 function connectServer(server, callback) {
-  var SERVER = conf.server.split(':');
+  const SERVER = conf.server.split(':');
   if(SERVER.length !== 2) return;
-  var HOST = SERVER[0];
-  var PORT = SERVER[1];
+  const HOST = SERVER[0];
+  const PORT = SERVER[1];
   SOCKET.connect(PORT, HOST, callback);
 }
 
@@ -57,9 +57,9 @@ function sendSystemData() {
   rpcClient.call('getSystemData', {}, function (err, data) {
     if(err) return;
     debug('getSystemData', JSON.stringify(data));
-    var monit = { type: 'system', data};
+    const monit = { type: 'system', data };
     SOCKET.write(JSON.stringify(monit));
-  })
+  });
 }
 
 function sendMonitorData() {
@@ -67,16 +67,16 @@ function sendMonitorData() {
   rpcClient.call('getMonitorData', {}, function (err, data) {
     if(err) return;
     debug('getMonitorData', JSON.stringify(data));
-    var monit = { type: 'monit', data};
+    const monit = { type: 'monit', data };
     SOCKET.write(JSON.stringify(monit));
-  })
+  });
 }
 
 if(conf.server) {
   connectServer(conf.server);
 }
 
-setInterval(function(){
+setInterval(function (){
   if(SOCKET.destroyed) {
     connectServer(conf.server, sendSystemData);
   } else {
